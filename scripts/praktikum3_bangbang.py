384ea87435cf498a1d9fff0c37489f57c40cbefc
kasutaja <galiguzovasveta@gmail.com>
2019-05-10 16:57:45 +0300

    prapraktikum7

--- scripts/praktikum3_bangbang.py
+++ scripts/praktikum3_bangbang.py
@@ -48,13 +48,17 @@ def move():
         ########################
         # YOUR CODE HERE START #
         ########################
-        vel_msg.linear.x = 0
+        p = 1
+        e = distances.centerMin - 0.6
+        vel_msg.linear.x = p*e
         vel_msg.linear.y = 0
         vel_msg.angular.z = 0
-        if distances.centerMin < 0.5:
-            vel_msg.linear.x = -0.1
-        else:
-            vel_msg.linear.x = 0.2
+#        if distances.centerMin < 0.45:
+#            vel_msg.linear.x = -0.1
+#        elif distances.centerMin > 0.55:
+#            vel_msg.linear.x = 0.1
+#        else:
+#            vel_msg.linear.x = 0
         velocity_publisher.publish(vel_msg)
         time.sleep(0.1)
         ######################
